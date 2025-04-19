65c8d622ceb0b854c295896f0d92cd69e2826e13
Pamella Gaiguer <pamella.gaiguer@gmail.com>
2025-04-19 00:17:03 -0300

    removing old blog mock data

--- src/components/blog/BlogCards.tsx
+++ src/components/blog/BlogCards.tsx
@@ -7,37 +7,6 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { BlogService, PostResumido } from '@/services/blog.service';
 
-const blogPosts = [
-  {
-    title: 'Agenciamento Artístico na Prática',
-    description:
-      'Entenda como funciona o trabalho de um agente artístico e o impacto na carreira de um talento.',
-    image: '/images/placeholder-talent.jpg',
-    slug: 'agenciamento-artistico-na-pratica',
-  },
-  {
-    title: 'Dicas para Criar um Material Atrativo',
-    description:
-      'Saiba como produzir fotos e vídeos que realmente vendem sua imagem como ator ou atriz.',
-    image: '/images/placeholder-talent.jpg',
-    slug: 'dicas-material-para-atores',
-  },
-  {
-    title: 'Marketing de Influência: Muito Além dos Números',
-    description:
-      'Como criar parcerias autênticas com marcas e se destacar como influenciador.',
-    image: '/images/placeholder-talent.jpg',
-    slug: 'marketing-de-influencia-autenticidade',
-  },
-  {
-    title: 'Por que Fazer Workshops com Frequência?',
-    description:
-      'Conheça os benefícios de manter-se sempre atualizado através de treinamentos e cursos.',
-    image: '/images/placeholder-talent.jpg',
-    slug: 'importancia-dos-workshops',
-  },
-];
-
 interface BlogCardsProps {
   filter: string;
 }
@@ -143,72 +112,6 @@ const BlogCards = ({ filter }: BlogCardsProps) => {
       </div>
     </motion.div>
   );
-
-  // return (
-  //   <section ref={ref} className="py-20 bg-gray-100 dark:bg-gray-900">
-  //     {/* <pre></pre>
-
-  //     <div className="container-section">
-  //       <motion.div
-  //         initial={{ opacity: 0, y: 20 }}
-  //         animate={isInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 20 }}
-  //         transition={{ duration: 0.6 }}
-  //         className="text-center mb-16"
-  //       >
-  //         <h2 className="heading-secondary text-black dark:text-white">Nosso Blog</h2>
-  //         <div className="w-20 h-1 bg-gradient-to-r from-black to-gray-500 dark:from-white dark:to-gray-500 mx-auto mt-2 mb-6"></div>
-  //         <p className="text-gray-700 dark:text-gray-300 max-w-3xl mx-auto">
-  //           Fique por dentro das novidades, tendências e dicas para impulsionar sua
-  //           carreira e seus projetos.
-  //         </p>
-  //       </motion.div>
-
-  //       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8">
-  //         {blogPosts.map((post, index) => (
-  //           <motion.div
-  //             key={index}
-  //             initial={{ opacity: 0, y: 30 }}
-  //             animate={isInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 30 }}
-  //             transition={{ duration: 0.6, delay: index * 0.1 }}
-  //             className="bg-white dark:bg-black p-0 rounded-lg shadow-md hover:shadow-lg transition-all duration-300"
-  //           >
-  //             <div className="flex justify-center items-center max-h-[250px] overflow-hidden bg-black/10">
-  //               <Image
-  //                 src={post.image || '/images/placeholder-talent.jpg'}
-  //                 alt={post.title}
-  //                 width={0}
-  //                 height={0}
-  //                 sizes="100vw"
-  //                 className="w-auto h-[250px] object-contain"
-  //               />
-  //             </div>
-  //             <div className="p-6 text-center">
-  //               <h3 className="text-xl font-semibold mb-3 text-black dark:text-white">
-  //                 {post.title}
-  //               </h3>
-  //               <p className="text-gray-600 dark:text-gray-400 mb-4">
-  //                 {post.description}
-  //               </p>
-  //               <Link
-  //                 href={`/blog/${post.slug}`}
-  //                 className="nav-link inline-block text-black dark:text-white font-medium"
-  //               >
-  //                 Leia mais
-  //               </Link>
-  //             </div>
-  //           </motion.div>
-  //         ))}
-  //       </div>
-
-  //       <motion.div
-  //         initial={{ opacity: 0, y: 20 }}
-  //         animate={isInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 20 }}
-  //         transition={{ duration: 0.6, delay: 0.6 }}
-  //         className="text-center mt-12"
-  //       ></motion.div>
-  //     </div> */}
-  //   </section>
-  // );
 
   return (
     <div className="container-section py-16">
