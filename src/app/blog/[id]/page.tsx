029765c913229079a53d2bb987ef1aedaaad6c85
Pamella Gaiguer <pamella.gaiguer@gmail.com>
2025-04-24 16:11:33 -0300

    blog: all data from api is pulled from api

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -9,6 +9,7 @@ import { FaArrowLeft } from 'react-icons/fa';
 import AnimatedSection from '@/components/shared/AnimatedSection';
 import AnimatedText from '@/components/shared/AnimatedText';
 import { BlogService } from '@/services/blog.service';
+import { formatarData } from '@/utils';
 
 type Tag = {
   id: number;
@@ -20,11 +21,14 @@ type Tag = {
 type Posts = {
   id: number;
   titulo: string;
+  slug?: string;
   resumo: string;
-  data_publicacao: string;
   imagem_destaque: string;
-  categoria_nome: string;
+  data_publicacao: string;
+  categoria?: number;
+  categoria_nome?: string;
   conteudo: string;
+  autor_nome?: string;
   tags: Tag[];
 };
 
@@ -140,194 +144,66 @@ export default function BlogPost() {
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-12">
           {/* Coluna da esquerda - Conteúdo principal */}
           <AnimatedSection className="lg:col-span-2" direction="right" delay={0.4}>
-            {/* Tabs */}
+            {/* Titulo */}
             <div className="border-b border-gray-200 dark:border-gray-800 mb-8">
               <h1>{post.titulo}</h1>
-              {/* <Text fs="italic">{post.resumo}</Text> */}
-              {/* {blog.experience && blog.experience.length > 0 && (
-                  <button
-                    onClick={() => setActiveTab('experiencia')}
-                    className={`py-4 px-1 border-b-2 font-medium text-sm hover-link ${
-                      activeTab === 'experiencia'
-                        ? 'border-black dark:border-white text-black dark:text-white'
-                        : 'border-transparent text-gray-500 dark:text-gray-400'
-                    }`}
-                  >
-                    Experiência
-                  </button>
-                )}
-
-                {blog.gallery && blog.gallery.length > 0 && (
-                  <button
-                    onClick={() => setActiveTab('galeria')}
-                    className={`py-4 px-1 border-b-2 font-medium text-sm hover-link ${
-                      activeTab === 'galeria'
-                        ? 'border-black dark:border-white text-black dark:text-white'
-                        : 'border-transparent text-gray-500 dark:text-gray-400'
-                    }`}
-                  >
-                    Galeria
-                  </button>
-                )} */}
             </div>
+            {/* Resumo e Data */}
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-12">
+              <div className="order-1">
+                <small>{post.resumo}</small>
+              </div>
+              <div className="order-2 text-right text-sm text-gray-500">
+                {formatarData(post.data_publicacao)}
+              </div>
+            </div>
+            {/* Conteúdo do Blog */}
+            <div>
+              <p>{post.conteudo}</p>
+            </div>
+            <div className="align-right"></div>
           </AnimatedSection>
 
           {/* Coluna da direita - Informações básicas */}
           <AnimatedSection className="lg:col-span-1" direction="left" delay={0.2}>
             <div className="bg-gray-100 dark:bg-gray-900 rounded-lg p-6 sticky top-24">
               <h2 className="text-2xl font-semibold text-black dark:text-white mb-6">
-                Informações
+                Detalhes
               </h2>
 
               <div className="space-y-4">
                 <div>
-                  <h3 className="text-sm text-gray-500 dark:text-gray-400">Idade</h3>
-                  {/* <p className="text-black dark:text-white">{blog.age} anos</p> */}
+                  <h3 className="text-sm text-gray-500 dark:text-gray-400">Tags:</h3>
+                  {post.tags.map((cat, index) => (
+                    <span
+                      key={index}
+                      className="bg-white/20 text-white px-3 py-1 rounded-full text-sm"
+                    >
+                      {cat.nome}
+                    </span>
+                  ))}
                 </div>
 
                 <div>
-                  <h3 className="text-sm text-gray-500 dark:text-gray-400">Altura</h3>
-                  {/* <p className="text-black dark:text-white">{blog.height}</p> */}
-                </div>
-
-                {/* {blog.measurements && (
-                  <>
-                    {blog.measurements.bust && (
-                      <div>
-                        <h3 className="text-sm text-gray-500 dark:text-gray-400">
-                          Busto
-                        </h3>
-                        <p className="text-black dark:text-white">
-                          {blog.measurements.bust}
-                        </p>
-                      </div>
-                    )}
-
-                    {blog.measurements.waist && (
-                      <div>
-                        <h3 className="text-sm text-gray-500 dark:text-gray-400">
-                          Cintura
-                        </h3>
-                        <p className="text-black dark:text-white">
-                          {blog.measurements.waist}
-                        </p>
-                      </div>
-                    )}
-
-                    {blog.measurements.hips && (
-                      <div>
-                        <h3 className="text-sm text-gray-500 dark:text-gray-400">
-                          Quadril
-                        </h3>
-                        <div className="prose dark:prose-invert max-w-none">
-                          {blog.bio && <p>{blog.bio}</p>}
-                        </div>
-                      </div>
-                    )}
-
-                    {blog.measurements.shoes && (
-                      <div>
-                        <h3 className="text-sm text-gray-500 dark:text-gray-400">
-                          Calçado
-                        </h3>
-                        <p className="text-black dark:text-white">
-                          {blog.measurements.shoes}
-                        </p>
-                      </div>
-                    )}
-
-                    {blog.measurements.eyes && (
-                      <div>
-                        <h3 className="text-sm text-gray-500 dark:text-gray-400">
-                          Olhos
-                        </h3>
-                        <p className="text-black dark:text-white">
-                          {blog.measurements.eyes}
-                        </p>
-                      </div>
-                    )}
-
-                    {blog.measurements.hair && (
-                      <div>
-                        <h3 className="text-sm text-gray-500 dark:text-gray-400">
-                          Cabelo
-                        </h3>
-                        <p className="text-black dark:text-white">
-                          {blog.measurements.hair}
-                        </p>
-                      </div>
-                    )}
-                  </>
-                )} */}
-
-                <div>
-                  <h3 className="text-sm text-gray-500 dark:text-gray-400">
-                    Especialidades
-                  </h3>
+                  <h3 className="text-sm text-gray-500 dark:text-gray-400">Autor:</h3>
                   <div className="flex flex-wrap gap-2 mt-1">
-                    {/* {blog.specialties.map((specialty: string, index: number) => (
-                      <span
-                        key={index}
-                        className="bg-gray-200 dark:bg-gray-800 text-black dark:text-white px-2 py-1 rounded text-xs"
-                      >
-                        {specialty}
-                      </span>
-                    ))} */}
+                    <p>{post.autor_nome}</p>
                   </div>
                 </div>
-
-                {/* {blog.social && (
-                  <div>
-                    <h3 className="text-sm text-gray-500 dark:text-gray-400 mb-2">
-                      Redes Sociais
-                    </h3>
-                    <div className="flex space-x-3">
-                      {blog.social.instagram && (
-                        <a
-                          href={blog.social.instagram}
-                          target="_blank"
-                          rel="noopener noreferrer"
-                          className="hover-link text-black dark:text-white"
-                          aria-label="Instagram"
-                        >
-                          <FaInstagram className="w-5 h-5" />
-                        </a>
-                      )}
-
-                      {blog.social.youtube && (
-                        <a
-                          href={blog.social.youtube}
-                          target="_blank"
-                          rel="noopener noreferrer"
-                          className="hover-link text-black dark:text-white"
-                          aria-label="YouTube"
-                        >
-                          <FaYoutube className="w-5 h-5" />
-                        </a>
-                      )}
-
-                      {blog.social.imdb && (
-                        <a
-                          href={blog.social.imdb}
-                          target="_blank"
-                          rel="noopener noreferrer"
-                          className="hover-link text-black dark:text-white"
-                          aria-label="IMDb"
-                        >
-                          <FaImdb className="w-5 h-5" />
-                        </a>
-                      )}
-                    </div>
+                <div>
+                  <h3 className="text-sm text-gray-500 dark:text-gray-400">Categoria:</h3>
+                  <div className="flex flex-wrap gap-2 mt-1">
+                    <p>{post.categoria_nome}</p>
                   </div>
-                )} */}
+                </div>
               </div>
 
               <div className="mt-8">
                 <button
-                  onClick={() => (window.location.href = '/contato?casting=' + blog.name)}
-                  className="btn-primary w-full"
+                  onClick={handleGoBack}
+                  className="align-right flex items-center gap-2 bg-black/50 hover:bg-black text-white px-4 py-2 rounded-md transition-colors"
                 >
-                  Contratar este casting
+                  <FaArrowLeft /> Voltar
                 </button>
               </div>
             </div>
